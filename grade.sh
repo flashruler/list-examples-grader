fb4cf8b3e9ce21fe390422a01ab13ccc4a781642
flashruler <44584657+flashruler@users.noreply.github.com>
2022-11-10 13:02:56 -0800

    idk if this works

--- grade.sh
+++ grade.sh
@@ -1,7 +1,21 @@
 # Create your grading script here
 
 set -e
-
 rm -rf student-submission
 git clone $1 student-submission
+cd student-submission
+ls
+echo "we are in student-submission"
+if test -f ListExamples.java
+then
+    echo "this file exists"
+    cp -v ListExamples.java ../
+else 
+    echo "incorrect file, please use the name ListExamples.java"
+    exit
+fi
+cd .. 
+echo "back in parent dir"
 
+javac -cp . lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+echo "compiled"
