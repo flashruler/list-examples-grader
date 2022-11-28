055d0e7d9770f96d5d31f007fb57693997fd53e2
Jay Buensuceso <jbuensuceso@ucsd.edu>
2022-11-27 21:39:50 -0800

    big code changes

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,12 +1,54 @@
-import static org.junit.Assert.*;
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.*;
-import org.junit.Test;
+import java.util.*;
+import static org.junit.Assert.*;
+
+class Checker implements StringChecker {
+  public boolean checkString(String s) {
+    return s.contains("test");
+  }
+}
 
 public class TestListExamples {
   // Write your grading tests here!
-  @Test
+  StringChecker s = new Checker();
+
+  @Test(timeout = 100)
+  public void filter_test() {
+    List<String> input = Arrays.asList(new String[] { "running", "test on", "redbull", "rn test" });
+    List<String> expect = Arrays.asList(new String[] { "test on", "rn test" });
+    List<String> result = ListExamples.filter(input, s);
+
+    Assert.assertArrayEquals(expect.toArray(), result.toArray());
+  }
+
+  @Test(timeout = 100)
+  public void filter_testDuplicates() {
+    List<String> input = Arrays.asList(new String[] { "test", "test", "test", "test", "test" });
+    List<String> expect = Arrays.asList(new String[] { "test", "test", "test", "test", "test" });
+    List<String> result = ListExamples.filter(input, s);
+
+    Assert.assertArrayEquals(expect.toArray(), result.toArray());
+  }
+
+  @Test(timeout = 100)
+  public void filter_testEmpty() {
+    List<String> input = Arrays.asList(new String[] {});
+    List<String> expect = Arrays.asList(new String[] {});
+    List<String> result = ListExamples.filter(input, s);
+
+    Assert.assertArrayEquals(expect.toArray(), result.toArray());
+  }
+
+  @Test(timeout = 100)
+  public void filter_testNoMatch() {
+    List<String> input = Arrays.asList(new String[] { "leclerc", "verstappen", "hamilton", "vettel" });
+    List<String> expect = Arrays.asList(new String[] {});
+    List<String> result = ListExamples.filter(input, s);
+
+    Assert.assertArrayEquals(expect.toArray(), result.toArray());
+  }
+
+  @Test(timeout = 100)
   public void testMerge() {
     List<String> input1 = new ArrayList<>();
     List<String> input2 = new ArrayList<>();
@@ -22,6 +64,34 @@ public class TestListExamples {
     // ListExamples.merge(input1, input2);
     assertEquals(expected, ListExamples.merge(input1, input2));
   }
+
+  @Test(timeout = 100)
+  public void testMergeDuplicate() {
+    List<String> input1 = new ArrayList<>();
+    List<String> input2 = new ArrayList<>();
+    List<String> expected = new ArrayList<>();
+    input1.add("a");
+    input1.add("c");
+    input1.add("d");
+    input2.add("a");
+    input2.add("c");
+    input2.add("d");
+    expected.add("a");
+    expected.add("a");
+    expected.add("c");
+    expected.add("c");
+    expected.add("d");
+    expected.add("d");
+    // ListExamples.merge(input1, input2);
+    assertEquals(expected, ListExamples.merge(input1, input2));
+  }
+
+  @Test(timeout = 100)
+  public void testMergeEmpty() {
+    List<String> input1 = new ArrayList<>();
+    List<String> input2 = new ArrayList<>();
+    List<String> expected = new ArrayList<>();
+    // ListExamples.merge(input1, input2);
+    assertEquals(expected, ListExamples.merge(input1, input2));
+  }
 }
-
-
--- grade.sh
+++ grade.sh
@@ -1,25 +1,41 @@
 # Create your grading script here
-
-set -e
+PTH=".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
 rm -rf student-submission
 git clone $1 student-submission
 cd student-submission
-ls
-echo "we are in student-submission"
 if test -f ListExamples.java
 then
-    echo "this file exists"
     cp -v ListExamples.java ../
 else 
-    echo "incorrect file, please use the name ListExamples.java"
+    echo "incorrect file, please use the correct naming/formatting"
     exit
 fi
 cd .. 
-echo "back in parent dir"
+javac -cp $PTH *.java 2> compile_error.txt
 
+if [[ $? -ne 0 ]]
+then
+    echo "Failed to compile."
+    cat compile_error.txt
+    exit 1
+else
+    echo "Sucessfully compiled..."
+fi
 
-javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
-echo "compiled"
+java -cp $PTH org.junit.runner.JUnitCore TestListExamples > Result.txt
+grep "Tests run:" < Result.txt > ResultScore.txt
+PASSED=`wc -l ResultScore.txt | grep -o '[0-9]'`
+VARS=(`grep -Eo '[0-9]{1,2}' < resultScore.txt`)
+TOTAL_TESTS=${VARS[0]}
+FAILURES=${VARS[1]}
+SUCCESS=$(($TOTAL_TESTS-$FAILURES))
 
-java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+if [[ $TOTAL_TESTS -eq $SUCCESS ]]
+then
+    echo "7/7 tests passed."
+else
+    echo "error"
+    echo "$(($TOTAL_TESTS-$FAILURES))/7 tests passed."
+fi
+cat result.txt
 
